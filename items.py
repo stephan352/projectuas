6bb916ee90f60ff0fb7d361c85f948260d139702
Stephan <stephanhd0@gmail.com>
2024-12-16 13:07:48 +0700

    change armor behaviour

--- items.py
+++ items.py
@@ -5,6 +5,15 @@ class Item():
 class Gear(Item):
     def __init__(self, game):
         super().__init__(game)
+        self.beingused = False
+
+    def useEffect(self):
+        self.game.player.armor += self.armorgain
+        self.beingused = True
+
+    def undoEffect(self):
+        self.game.player.armor -= self.armorgain
+        self.beingused = False
 
 class Weapon(Item):
     def __init__(self, game):
@@ -15,20 +24,16 @@ class Kevlar(Gear):
     def __init__(self, game):
         super().__init__(game)
         self.armorgain = 1
-        self.beingused = False
-    
-    def useEffect(self):
-        self.game.player.armor += self.armorgain
-        self.beingused = True
-
-    def undoEffect(self):
-        self.game.player.armor -= self.armorgain
-        self.beingused = False
 
 class Machete(Weapon):
     def __init__(self, game):
         super().__init__(game)
         self.damage = 20
+
+class KitchenKnife(Weapon):
+    def __init__(self, game):
+        super().__init__(game)
+        self.damage = 15
 
 weaponTypes = Weapon.__subclasses__()
 gearTypes = Gear.__subclasses__()
